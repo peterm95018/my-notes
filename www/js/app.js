a2fdb6cc05d7ae54ab20dd5659dcc277b7fd9e5c
Peter McMillan <peterm95018@gmail.com>
2016-01-20 15:57:49 -0800

    reordering functionality

--- www/js/app.js
+++ www/js/app.js
@@ -33,10 +33,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
 app.controller('ListCtrl', function($scope, NoteStore) {
   $scope.notes = NoteStore.list();
+  $scope.reordering = false;
 
   $scope.remove = function(noteId) {
     NoteStore.remove(noteId);
-  }
+  };
+
+  $scope.move = function(note, fromIndex, toIndex) {
+    NoteStore.move(note, fromIndex, toIndex);
+  };
+
+  $scope.toggleReordering = function() {
+    $scope.reordering = !$scope.reordering;
+  };
+
 });
 
 
